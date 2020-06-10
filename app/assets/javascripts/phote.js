a12d2ebffe660459fe649b4cea1dd729226018d9
mushakora <mushakora89br@yahoo.co.jp>
2020-06-10 14:02:08 +0900

    [add]いいね機能の非同期

--- app/assets/javascripts/phote.js
+++ app/assets/javascripts/phote.js
@@ -1,3 +1,3 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
   $('.top_phote').hide().fadeIn(2000);
 });
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-      item = Item.find(params[:item_id])
-      favorite = current_user.favorites.new(item_id: item.id)
+      @item = Item.find(params[:item_id])
+      favorite = current_user.favorites.new(item_id: @item.id)
       favorite.save
-      redirect_back(fallback_location: root_path)
   end
 
   def destroy
-  	  item = Item.find(params[:item_id])
-  	  favorite = current_user.favorites.find_by(item_id: item.id)
+  	  @item = Item.find(params[:item_id])
+  	  favorite = current_user.favorites.find_by(item_id: @item.id)
   	  favorite.destroy
-  	  redirect_back(fallback_location: root_path)
   end
 
   private
