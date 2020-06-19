75536176cb46c69196d7d1f35637e164cca1cec3
mushakora <mushakora89br@yahoo.co.jp>
2020-06-19 21:47:50 +0900

    [add]ページトップに戻るボタンの追加

--- app/assets/javascripts/phote.js
+++ app/assets/javascripts/phote.js
@@ -40,3 +40,23 @@ function generateDay(obj) {
     }
   }
 }
+
+// トップに戻るボタンの設定 //
+$(function() {
+  $('#back a').on('click',function(){
+    $('body, html').animate({
+      scrollTop:0
+    }, 500);
+      return false;
+  });
+});
+
+$(function(){
+  $('#back a').mouseover(function(){
+      $('#back a').addClass('back-btn');
+  });
+
+  $('#back a').mouseout(function(){
+      $('#back a').removeClass('back-btn');
+  });
+});
